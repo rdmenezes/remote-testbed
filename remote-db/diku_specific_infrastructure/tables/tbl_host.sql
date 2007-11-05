3659c40347fa64549464cac2fd1aa1fc1d59ffe8
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2007-11-05 10:47:19 +0000

    issue 5: increase the size of host.ip to support IPv6 addresses
    Increase the size of the ip field of the host table from 15 to 46 to
    make it possible to store IPv6 addresses. This update is mostly a
    preparation for allowing the MCI module to later be updated to support
    IPv6.

--- remote-db/diku_specific_infrastructure/tables/tbl_host.sql
+++ remote-db/diku_specific_infrastructure/tables/tbl_host.sql
@@ -3,6 +3,6 @@ drop table if exists host;
 CREATE TABLE host (
 	`id` int(11) unsigned NOT NULL auto_increment,
 	`dnsname` varchar(255) default '',
-	`ip` varchar(15) default '',
+	`ip` varchar(46) default '',
 	PRIMARY KEY  (`id`)
 ) ENGINE = InnoDB;
