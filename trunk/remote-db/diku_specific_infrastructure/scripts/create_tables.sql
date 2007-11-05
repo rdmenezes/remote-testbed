4fb30ba7c7299863cb38a67331ea1299f3522bac
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2007-11-05 20:15:12 +0000

    Issue 10: remove the macaddress and tosaddress tables
    As of commit 1aacb273db84a35d515709b865a418b3424b6684 in the remote-mci
    repository, they are no longer used or required.

--- trunk/remote-db/diku_specific_infrastructure/scripts/create_tables.sql
+++ trunk/remote-db/diku_specific_infrastructure/scripts/create_tables.sql
@@ -1,8 +1,6 @@
 set FOREIGN_KEY_CHECKS=0;
 
 source diku_specific_infrastructure/tables/tbl_host.sql;
-source diku_specific_infrastructure/tables/tbl_macaddress.sql;
 source diku_specific_infrastructure/tables/tbl_path.sql;
-source diku_specific_infrastructure/tables/tbl_tosaddress.sql;
 
 set FOREIGN_KEY_CHECKS=1;
