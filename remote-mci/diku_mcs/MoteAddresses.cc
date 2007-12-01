28eeeaecb94695d548e347db13738c8ae7e43ec8
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2007-12-01 14:48:01 +0000

    Store TOS address as std::string in MoteAddresses

--- remote-mci/diku_mcs/MoteAddresses.cc
+++ remote-mci/diku_mcs/MoteAddresses.cc
@@ -2,7 +2,7 @@
 
 namespace remote { namespace diku_mcs {
 
-MoteAddresses::MoteAddresses(std::string p_mac, uint16_t p_tosAddress)
+MoteAddresses::MoteAddresses(std::string p_mac, std::string p_tosAddress)
               : tosAddress(p_tosAddress), mac(p_mac)
 {
 }
