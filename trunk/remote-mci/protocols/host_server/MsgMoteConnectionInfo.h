8d81ca57b5d9a9d57ad1ea145296d8e066bc981d
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-03-11 19:48:01 +0000

    Reorganize to remove DIKU specific naming
    It is more beneficial to keep the MCI generic. The reorganization goes
    something like this:
    
     - Move diku_mch/ directory to mch/.
     - Move diku_mcs/ files to existing mcs/ directory.
     - Rename diku_{mch,mcs} binaries to remote-{mch,mcs}
     - Rename MCH helper plugpiper binary to remote-mch-notify.
     - Move protocols/diku_host_server/ to protocols/host_server/.
     - Strip diku_ from namespaces.
     - Rename default option paths to use remote- prefix instead of diku_.
     - Strip diku- from configure options.
    
    Contrib example files are updated accordingly. In addition the email
    mentioned in configure was changed to remote-testbed@googlegroups.com.

---
+++ trunk/remote-mci/protocols/host_server/MsgMoteConnectionInfo.h
@@ -0,0 +1,36 @@
+#ifndef MSGMOTECONNECTIONINFO_H_
+#define MSGMOTECONNECTIONINFO_H_
+
+#include "BaseMsg.h"
+#include "MsgPayload.h"
+
+namespace remote { namespace protocols { namespace host_server {
+
+class MsgMoteConnectionInfo : public BaseMsg
+{
+	public:
+		MsgMoteConnectionInfo();
+		MsgMoteConnectionInfo(std::string p_mac, std::string p_path,
+				      std::string p_platform);
+		virtual ~MsgMoteConnectionInfo();
+		void operator = (const MsgMoteConnectionInfo& o);
+
+		uint32_t getLength();
+		uint8_t* write(uint8_t* buffer, uint32_t& buflen);
+		uint8_t* read(uint8_t* buffer, uint32_t& buflen);
+		void print(FILE* s);
+
+		void read(int fd);
+		void write(int fd);
+		MsgPayload& getPath();
+		std::string getMac();
+		std::string getPlatform();
+	private:
+		MsgPayload mac;
+		MsgPayload path;
+		MsgPayload platform;
+};
+
+}}}
+
+#endif /*MSGMOTECONNECTIONINFO_H_*/
