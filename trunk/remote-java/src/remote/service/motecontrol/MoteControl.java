b48f7d8b52e8c761f51c167e4a24a1067dec2ff8
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-05-27 05:13:55 +0000

    Rename and document MoteControlResult; empty ServiceManager

--- trunk/remote-java/src/remote/service/motecontrol/MoteControl.java
+++ trunk/remote-java/src/remote/service/motecontrol/MoteControl.java
@@ -8,55 +8,55 @@ public interface MoteControl extends Service {
 
 	/** Get status for motes.
 	 *
-	 * On success, the result object is an array of {@link CommandResult}
-	 * objects (CommandResult[]) with an entry for each mote.
+	 * On success, the result object is an array of {@link MoteControlResult}
+	 * objects (MoteControlResult[]) with an entry for each mote.
 	 *
 	 * @param session	ID of the session that wants privileges.
-	 * @param motes		IDs of mote to control.
+	 * @param motes		IDs of motes for which to get status.
 	 * @param callback	Callback for receiving result or error.
 	 */
 	void getStatus(String session, long[] motes, ServiceCallback callback);
 
 	/** Start motes.
 	 *
-	 * On success, the result object is an array of {@link CommandResult}
-	 * objects (CommandResult[]) with an entry for each mote.
+	 * On success, the result object is an array of {@link MoteControlResult}
+	 * objects (MoteControlResult[]) with an entry for each mote.
 	 *
 	 * @param session	ID of the session that wants privileges.
-	 * @param motes		IDs of mote to control.
+	 * @param motes		IDs of motes to control.
 	 * @param callback	Callback for receiving result or error.
 	 */
 	void start(String session, long[] motes, ServiceCallback callback);
 
 	/** Stop motes.
 	 *
-	 * On success, the result object is an array of {@link CommandResult}
-	 * objects (CommandResult[]) with an entry for each mote.
+	 * On success, the result object is an array of {@link MoteControlResult}
+	 * objects (MoteControlResult[]) with an entry for each mote.
 	 *
 	 * @param session	ID of the session that wants privileges.
-	 * @param motes		IDs of mote to control.
+	 * @param motes		IDs of motes to control.
 	 * @param callback	Callback for receiving result or error.
 	 */
 	void stop(String session, long[] motes, ServiceCallback callback);
 
 	/** Reset motes.
 	 *
-	 * On success, the result object is an array of {@link CommandResult}
-	 * objects (CommandResult[]) with an entry for each mote.
+	 * On success, the result object is an array of {@link MoteControlResult}
+	 * objects (MoteControlResult[]) with an entry for each mote.
 	 *
 	 * @param session	ID of the session that wants privileges.
-	 * @param motes		IDs of mote to control.
+	 * @param motes		IDs of motes to control.
 	 * @param callback	Callback for receiving result or error.
 	 */
 	void reset(String session, long[] motes, ServiceCallback callback);
 
 	/** Program motes.
 	 *
-	 * On success, the result object is an array of {@link CommandResult}
-	 * objects (CommandResult[]) with an entry for each mote.
+	 * On success, the result object is an array of {@link MoteControlResult}
+	 * objects (MoteControlResult[]) with an entry for each mote.
 	 *
 	 * @param session	ID of the session that wants privileges.
-	 * @param motes		IDs of mote to control.
+	 * @param motes		IDs of motes to control.
 	 * @param program	Binary to use when programming.
 	 * @param callback	Callback for receiving result or error.
 	 */
@@ -66,11 +66,11 @@ public interface MoteControl extends Service {
 	 *
 	 * Can be used to cancel a previous programming request
 	 * if it takes too long. On success, the result object
-	 * is an array of {@link CommandResult} objects
-	 * (CommandResult[]) with an entry for each mote.
+	 * is an array of {@link MoteControlResult} objects
+	 * (MoteControlResult[]) with an entry for each mote.
 	 *
 	 * @param session	ID of the session that wants privileges.
-	 * @param motes		IDs of mote to control.
+	 * @param motes		IDs of motes to control.
 	 * @param callback	Callback for receiving result or error.
 	 */
 	void cancel(String session, long[] motes, ServiceCallback callback);
