22264222f1b9a8083c2c70c3557ff1abf61ef21f
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-05-31 11:31:18 +0000

    Remove UNKNOWN mote command type and fix MoteCommand accordingly

--- trunk/remote-java/src/remote/control/MoteCommand.java
+++ trunk/remote-java/src/remote/control/MoteCommand.java
@@ -1,12 +1,11 @@
 package remote.control;
 
-/**
+/** Mote command container.
  *
+ * Wrapper class for low-level mote command type.
  */
 public class MoteCommand {
 
-	/** Unknown last command. */
-	public static final MoteCommand UNKNOWN = new MoteCommand(MoteCommandType.UNKNOWN);
 	/** Reset the mote. */
 	public static final MoteCommand RESET = new MoteCommand(MoteCommandType.RESET);
 	/** Start the mote. */
@@ -17,6 +16,8 @@ public class MoteCommand {
 	public static final MoteCommand PROGRAM = new MoteCommand(MoteCommandType.PROGRAM);
 	/** Cancel the last command. */
 	public static final MoteCommand CANCEL = new MoteCommand(MoteCommandType.CANCEL);
+	/** Unknown (last) command. */
+	public static final MoteCommand UNKNOWN = new MoteCommand(MoteCommandType.TYPES);
 	/** The low-level command value. */
 	private int command;
 
