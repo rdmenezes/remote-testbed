59a915b1df3b1979fcba053cb89e19eb905abbb5
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-05-31 11:31:18 +0000

    Remove UNKNOWN mote command type and fix MoteCommand accordingly

--- remote-java/src/remote/control/MoteCommandType.java
+++ remote-java/src/remote/control/MoteCommandType.java
@@ -2,10 +2,17 @@ package remote.control;
 
 /** Low-level mote command enumeration type. */
 public interface MoteCommandType {
-	int UNKNOWN = 0;
-	int RESET   = 1;
-	int START   = 2;
-	int STOP    = 3;
-	int PROGRAM = 4;
-	int CANCEL  = 5;
+	/** Start command. */
+	int START   = 0;
+	/** Stop command.*/
+	int STOP    = 1;
+	/** Reset command. */
+	int RESET   = 2;
+	/** Program command. */
+	int PROGRAM = 3;
+	/** Cancel command. */
+	int CANCEL  = 4;
+
+	/** Number of command types. */
+	int TYPES   = 5;
 }
