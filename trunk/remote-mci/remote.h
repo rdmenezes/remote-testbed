8225bf5df10d4fa2e2e8b6794cd32eb1453c9c68
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-06-05 14:29:56 +0000

    MCH: Add top-level remote.h file for including system and util files
    Also, reorder include files so system header files comes before library
    files, which comes before project library files, followed by the program
    header files.

---
+++ trunk/remote-mci/remote.h
@@ -0,0 +1,34 @@
+#ifndef REMOTE_REMOTE_H
+#define REMOTE_REMOTE_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#include "libutil/File.h"
+#include "libutil/Log.h"
+#include "libutil/Config.h"
+#include "libutil/error.h"
+
+#endif
