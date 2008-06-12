b09faa37313e9e1169b19cef46bee0d4443fdc44
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-06-12 10:45:21 +0000

    Use generics for the ServiceCallback result type

--- remote-java/src/remote/service/ServiceCallback.java
+++ remote-java/src/remote/service/ServiceCallback.java
@@ -5,8 +5,12 @@ package remote.service;
  * An callback interface used for signaling success or failure of
  * a service call. Inspired by GWT's AsyncCallback and Axis2's
  * AxisCallback interface.
+ * 
+ * @param <T> Type of the service result object.
+ * @see <a href="http://ws.apache.org/axis2/1_3/api/org/apache/axis2/client/async/AxisCallback.html">org.apache.axis2.client.async.AxisCallback</a>
+ * @see <a href="http://google-web-toolkit.googlecode.com/svn/javadoc/1.4/com/google/gwt/user/client/rpc/AsyncCallback.html">com.google.gwt.user.client.rpc.AsyncCallback</a>
  */
-public interface ServiceCallback {
+public interface ServiceCallback<T> {
 
 	/** Success callback.
 	 *
@@ -14,7 +18,7 @@ public interface ServiceCallback {
 	 *
 	 * @param result	Result object.
 	 */
-	void onSuccess(Object result);
+	void onSuccess(T result);
 
 	/** Error callback.
 	 *
