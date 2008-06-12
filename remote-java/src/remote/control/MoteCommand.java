84556592ed39f838d8c70ccfeecc4b45fc0482d8
jonas.fonseca <jonas.fonseca@60cecc47-b838-0410-ae0c-1bc71cdb0aa7>
2008-06-12 09:29:01 +0000

    Make the constructor of the Mote{Command,Result,Status} enum types private
    ... and sprinkle with final keyword. Inspired by Chapter 5 of Effective Java.

--- remote-java/src/remote/control/MoteCommand.java
+++ remote-java/src/remote/control/MoteCommand.java
@@ -8,20 +8,26 @@ public class MoteCommand {
 
 	/** Reset the mote. */
 	public static final MoteCommand RESET = new MoteCommand(MoteCommandType.RESET);
+
 	/** Start the mote. */
 	public static final MoteCommand START = new MoteCommand(MoteCommandType.START);
+
 	/** Stop the mote. */
 	public static final MoteCommand STOP = new MoteCommand(MoteCommandType.STOP);
+
 	/** Program the mote. */
 	public static final MoteCommand PROGRAM = new MoteCommand(MoteCommandType.PROGRAM);
+
 	/** Cancel the last command. */
 	public static final MoteCommand CANCEL = new MoteCommand(MoteCommandType.CANCEL);
+
 	/** Unknown (last) command. */
 	public static final MoteCommand UNKNOWN = new MoteCommand(MoteCommandType.TYPES);
-	/** The low-level command value. */
-	private int command;
 
-	MoteCommand(int command)
+	/** The low-level command value. */
+	private final int command;
+
+	private MoteCommand(int command)
 	{
 		this.command = command;
 	}
--- remote-java/src/remote/control/MoteStatus.java
+++ remote-java/src/remote/control/MoteStatus.java
@@ -4,39 +4,33 @@ package remote.control;
 public class MoteStatus {
 
 	/** The status of the mote is unknown. */
-	public static final MoteStatus UNKNOWN     = new MoteStatus(MoteStatusType.UNKNOWN);
+	public static final MoteStatus UNKNOWN     = new MoteStatus(MoteStatusType.UNKNOWN, "unknown");
 
 	/** The mote is no longer available. */
-	public static final MoteStatus UNAVAILABLE = new MoteStatus(MoteStatusType.UNAVAILABLE);
+	public static final MoteStatus UNAVAILABLE = new MoteStatus(MoteStatusType.UNAVAILABLE, "unavailable");
 
 	/** The mote is powered off. */
-	public static final MoteStatus STOPPED     = new MoteStatus(MoteStatusType.STOPPED);
+	public static final MoteStatus STOPPED     = new MoteStatus(MoteStatusType.STOPPED, "stopped");
 
 	/** The mote is running. */
-	public static final MoteStatus RUNNING     = new MoteStatus(MoteStatusType.RUNNING);
+	public static final MoteStatus RUNNING     = new MoteStatus(MoteStatusType.RUNNING, "running");
 
 	/** The mote is being programmed. */
-	public static final MoteStatus PROGRAMMING = new MoteStatus(MoteStatusType.PROGRAMMING);
+	public static final MoteStatus PROGRAMMING = new MoteStatus(MoteStatusType.PROGRAMMING, "programming");
 
-	private int status;
+	private final int status;
+	private final String string;
 
-	public MoteStatus(int status)
+	private MoteStatus(int status, String string)
 	{
 		this.status = status;
+		this.string = string;
 	}
 
 	@Override
 	public String toString()
 	{
-		if (this == UNAVAILABLE)
-			return "unavailable";
-		if (this == STOPPED)
-			return "stopped";
-		if (this == RUNNING)
-			return "running";
-		if (this == PROGRAMMING)
-			return "programming";
-		return "unknown";
+		return string;
 	}
 
 }
